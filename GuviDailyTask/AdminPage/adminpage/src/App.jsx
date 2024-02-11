f6fb0432acc8ecf4b1b8d19f4386177285891d6f
NishaAshokan <madhinishaashokan207@gmail.com>
2024-02-11 21:02:06 +0530

    Library

---
+++ GuviDailyTask/AdminPage/adminpage/src/App.jsx
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import './App.css';
+import './App2.css'
+import BookManagement from './BookManagement';
+import AuthorManagement from './AuthorManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Library store</h1>
+      <div className="dashboard">
+        <BookManagement />
+        <AuthorManagement />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ GuviDailyTask/AdminPage/adminpage/src/AuthorManagement.jsx
@@ -0,0 +1,34 @@
+
+import React, { useState } from 'react';
+import AuthorForm from './AuthorForm';
+
+const AuthorManagement = () => {
+  const [authors, setAuthors] = useState([]);
+
+  const addAuthor = (author) => {
+    setAuthors([...authors, author]);
+  };
+
+  const deleteAuthor = (index) => {
+    const newAuthors = [...authors];
+    newAuthors.splice(index, 1);
+    setAuthors(newAuthors);
+  };
+
+  return (
+    <div className="management-panel">
+      <h2>Author Management</h2>
+      <AuthorForm addAuthor={addAuthor} />
+      <ul>
+        {authors.map((author, index) => (
+          <li key={index}>
+            {author.name} - {author.birthDate}
+            <button onClick={() => deleteAuthor(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AuthorManagement;
