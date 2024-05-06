15d7cdd94ec707cac84464fa25c3a3033b1569b3
NishaAshokan <madhinishaashokan207@gmail.com>
2024-05-06 22:49:27 +0530

    new origin

--- DONE-AH/Backend/index.js
+++ DONE-AH/Backend/index.js
@@ -25,7 +25,7 @@ require('./db')
 app.use(bodyParser.json());
 //const allowedOrigins = ['http://localhost:3000', 'https://fitnessproject-api.vercel.app/'];
 
-const allowedOrigins = ['https://added-madhis-projects.vercel.app']; // Add more origins as needed
+const allowedOrigins = ['https://added-madhis-projects.vercel.app' , 'http://localhost:3000']; // Add more origins as needed
 
 app.use(
     cors({
--- Fitness-capstone/fitness-frontend/fitnessfreak/src/workout/[type]/Page.jsx
+++ Fitness-capstone/fitness-frontend/fitnessfreak/src/workout/[type]/Page.jsx
@@ -37,6 +37,37 @@ const Page = () => {
                     }
                 ]
             };
+            {
+                type: 'Abs',
+                imageUrl: 'https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80',
+                durationInMin: 40,
+                exercises: [
+                    {
+                        exercise: 'Flat Bench Press',
+                        videoUrl: 'https://gymvisual.com/img/p/1/7/5/5/2/17552.gif',
+                        sets: 3,
+                        reps: 10,
+                        rest: 60,
+                        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                    },
+                    {
+                        exercise: 'Incline Bench Press',
+                        videoUrl: 'https://gymvisual.com/img/p/1/0/3/9/8/10398.gif',
+                        sets: 3,
+                        reps: 10,
+                        rest: 60,
+                        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                    },
+                    {
+                        exercise: 'Decline Bench Press',
+                        videoUrl: 'https://gymvisual.com/img/p/6/5/2/3/6523.gif',
+                        sets: 3,
+                        reps: 10,
+                        rest: 60,
+                        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+                    }
+                ]
+            };
             setWorkout(data);
         };
 
