82342f35557208c67b6e20c091780986dc9fca33
NishaAshokan <madhinishaashokan207@gmail.com>
2024-06-04 13:34:15 +0530

    finallll

--- DONE-AH/Backend/index.js
+++ DONE-AH/Backend/index.js
@@ -23,8 +23,7 @@ require('dotenv').config();
 require('./db')
 
 app.use(bodyParser.json());
-//const allowedOrigins = ['http://localhost:3000', 'https://fitnessproject-api.vercel.app/'];
-
+//const allowedOrigins = ['http://localhost:3000',  'https://fitnessproject-api.vercel.app/'];
 const allowedOrigins = [ 'https://added-4qnd-git-master-madhis-projects.vercel.app', 'https://add-admin-695y-git-main-madhis-projects.vercel.app',  'http://localhost:3001' , 'http://localhost:3000' , 'http://localhost:3002', 'https://added-madhis-projects.vercel.app' ]; // Add more origins as needed
 
 app.use(
